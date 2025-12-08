fc7ba4316d597e12ed064445cefc31e764854065
Lisa Fontana <lisa.fontana1996@gmail.com>
2025-12-08 21:53:35 +1100

    change

--- ANALYSIS/Centrality/network metrics 2023 roosts.R
+++ ANALYSIS/Centrality/network metrics 2023 roosts.R
@@ -2,7 +2,7 @@
 library(sna)
 
 ##night time metrics
-net_night<-read.csv("~/Library/CloudStorage/OneDrive-AustralianNationalUniversity/People/Lisa Fontana/network_movement_night_2023_all_roosts.csv", header=T, row.names=1)
+net_night<-read.csv("~network_movement_night_2023_all_roosts.csv", header=T, row.names=1)
 
 ##subset it to the sites of interest here, if you want (this would change the metrics as it would only include their relationships to the other sites). The below is on all sites altogether
 
@@ -12,10 +12,10 @@ data_output_night$WEIGHTED_DEGREE<-degree(as.matrix(net_night), gmode="graph", c
 data_output_night$UNWEIGHTED_DEGREE<-degree(as.matrix(net_night), gmode="graph", cmode="freeman", rescale=FALSE, ignore.eval=TRUE)
 data_output_night$BETWEENESS<-betweenness(as.matrix(net_night), gmode="graph", rescale=FALSE)
 
-write.csv(data_output_night,"~/Library/CloudStorage/OneDrive-AustralianNationalUniversity/People/Lisa Fontana/day_network_metrics.csv")
+write.csv(data_output_night,"~/day_network_metrics.csv")
 
 ##day time metrics
-net_day<-read.csv("~/Library/CloudStorage/OneDrive-AustralianNationalUniversity/People/Lisa Fontana/network_movement_day_2023_all_roosts.csv", header=T, row.names=1)
+net_day<-read.csv("~/network_movement_day_2023_all_roosts.csv", header=T, row.names=1)
 
 ##again, subset it to the sites of interest here, if you want (this would change the metrics as it would only include their relationships to the other sites). The below is on all sites altogether
 
@@ -23,9 +23,8 @@ SITES_DAY<-rownames(net_day)
 data_output_day<-as.data.frame(SITES_DAY)
 data_output_day$WEIGHTED_DEGREE<-degree(as.matrix(net_day), gmode="graph", cmode="freeman", rescale=FALSE)
 data_output_day$UNWEIGHTED_DEGREE<-degree(as.matrix(net_day), gmode="graph", cmode="freeman", rescale=FALSE, ignore.eval=TRUE)
-data_output_day$BETWEENESS<-betweenness(as.matrix(net_day), gmode="graph", rescale=FALSE)
 
-write.csv(data_output_day,"~/Library/CloudStorage/OneDrive-AustralianNationalUniversity/People/Lisa Fontana/night_network_metrics.csv")
+#write.csv(data_output_day,"~/night_network_metrics.csv")
 
 
 
